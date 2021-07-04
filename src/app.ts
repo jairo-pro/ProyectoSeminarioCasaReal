8a7ce13405a7a65bbcefea38d5b1b00b281eb24a
Jairo Flores Z <jhairo4121@gmail.com>
2021-07-04 18:37:19 -0400

    Api

---
+++ src/app.ts
@@ -0,0 +1,41 @@
+import express, { Express } from "express";
+import * as bodyParser from "body-parser";
+import UserModules from "./modules/usermodule/init";
+import mongoose, { Mongoose } from "mongoose";
+import FileUpload from "express-fileupload";
+class App {
+  public app: Express = express();
+  public mongooseClient: Mongoose;
+  constructor() {
+    this.configuration();
+    this.connectDatabase();
+    this.initApp();
+  }
+  public connectDatabase() {
+    let host: string = "mongodb://172.18.0.2:27017";
+    let database: string = process.env.DATABASE || "seminario";
+    let connectionString: string = `${host}/${database}`;
+    mongoose.connect(connectionString, {
+      useNewUrlParser: true,
+    });
+    //Eventos
+    mongoose.connection.on("error", (err) => {
+      console.log("Connection Fail");
+      console.log(err);
+    });
+    mongoose.connection.on("open", () => {
+      console.log("database connection success!");
+    });
+    this.mongooseClient = mongoose;
+  }
+  public configuration() {
+    this.app.use(bodyParser.json());
+    this.app.use(bodyParser.urlencoded({ extended: false }));
+    this.app.use(FileUpload({ limits: { fileSize: 50 * 1024 * 1024 } }));
+  }
+  public initApp() {
+    console.log("LOAD MODULES");
+    const userModule = new UserModules("/api", this.app);
+  }
+}
+export default new App();
---
+++ src/modules/usermodule/routes.ts
@@ -0,0 +1,53 @@
+import RoutesController from "./routeController/RoutesController";
+import jsonwebtokenSecurity from "./middleware";
+import { Express } from "express";
+class Routes {
+  private routesController: RoutesController;
+  private routeparent: string;
+  constructor(routeparent: string, app: Express) {
+    this.routesController = new RoutesController();
+    this.routeparent = routeparent;
+    this.configureRoutes(app);
+  }
+  private configureRoutes(app: Express) {
+    //**--USER ROUTES--------------------------------------------------------------------------------------- */
+    app.route(`${this.routeparent}/login`).post(this.routesController.login);
+    app
+      .route(`${this.routeparent}/users`)
+      .post(this.routesController.createUsers);
+    app
+      .route(`${this.routeparent}/users`)
+      .get(this.routesController.getUsers);
+    app
+      .route(`${this.routeparent}/users/:id`)
+      .put(this.routesController.updateUsers);
+    app
+      .route(`${this.routeparent}/users/:id`)
+      .delete(this.routesController.removeUsers);
+    app
+      .route(`${this.routeparent}/uploadportrait/:id`)
+      .post(this.routesController.uploadPortrait);
+    app
+      .route(`${this.routeparent}/getportrait/:id`)
+      .get(this.routesController.getPortrait);
+
+    app
+      .route(`${this.routeparent}/addrol/:id`)
+      .put(this.routesController.addRol);
+    app
+      .route(`${this.routeparent}/removerol/:id`)
+      .put(this.routesController.removeUserRol);
+
+    //**--ROLES ROUTES--------------------------------------------------------------------------------------- */
+    app
+      .route(`${this.routeparent}/roles`)
+      .post(this.routesController.createRol);
+    app
+      .route(`${this.routeparent}/roles/:id`)
+      .delete(this.routesController.removeRol);
+    app
+      .route(`${this.routeparent}/roles/`)
+      .get(this.routesController.getRoles);
+  }
+}
+export default Routes;
